ca935e92f0d9d8d297e754920e64c037d913a861
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2022-04-07 19:44:49 +0000

    Update

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    $msisdn = $_GET['msisdn'];
+    $sequence_ID = $_GET['sequenceID'];
+    $data = $_GET['data'];
+    $type = 1;
+
+  
+    $mereply = "Hello David Sampah";
+    echo $mereply.'|'.$msisdn.'|'.$sequence_id.'|'.$type;
+
+?>
